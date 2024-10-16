2ef978fd58ab3a05254bdcd60132ea13acf7686e
anedfranca <anedourado@icloud.com>
2024-10-16 18:43:05 -0300

    implementando add e remove

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+def listar():
+    for chave in agenda:
+        for valor in agenda[chave]:
+            print(valor)
+        print()
+
 agenda = {
     "a": ["ane"] ,
     "b": ["bruna", "bianca"],
     "c": ["camilo"]
 }
 
-for chave in agenda:
-    for valor in agenda[chave]:
-        print(valor)
+entrada = input("Digite o seu contato: ") # ana
+inicial = entrada[0] # "a"
+
+agenda[inicial].append(entrada)
+
+listar()
+
+contato_remove = input("Digite o contato para remover: ")
+inicial_de_quem_eu_quero_remover = contato_remove[0]
+
+agenda[inicial_de_quem_eu_quero_remover].remove(contato_remove)
+
+listar()
